5edff11036cb136e1ff09fc7526b046299cefee8
Maple Observer <mapleobserver@gmail.com>
2019-07-15 19:48:51 +0800

    简化追捕模块

--- 自动日常.lua
+++ 自动日常.lua
@@ -6,9 +6,9 @@
     ($WudaoManualMaxLevel) = 84
 [if] (WudaoWaitCDExp) == null
     ($WudaoWaitCDExp) = ^none
+#input ($WudaoWaitCDExp)=打塔等待以下技能冷却,(WudaoWaitCDExp)
 #input ($WudaoWaitCDLevel)=从此层开始，等待技能冷却,(WudaoWaitCDLevel)
 #input ($WudaoManualMaxLevel)=从此层开始快速扫荡,(WudaoManualMaxLevel)
-#input ($WudaoWaitCDExp)=等待以下技能冷却,(WudaoWaitCDExp)
 // 追捕自定义参数
 [if] (ZBWaitCD) == null
     ($ZBWaitCD) = 15
@@ -16,36 +16,18 @@
     ($ZBMax) = 52
 [if] (ZBcdskill) == null
     ($ZBcdskill) = ^none
-[if] (ZBeq) == null
-    ($ZBeq) = 0
-[if] (AZBeq) == null
-    ($AZBeq) = 0
-[if] (ZBskill) == null||(ZBskill) == ^none
-    ($ZBskill) = 0,0,0,0,0,0,0,0,0,0
-[if] (AZBskill) == null
-    ($AZBskill) = 0,0,0,0,0,0,0,0,0,0
-[if] (ZBauto_pfm) == null
-    ($ZBauto_pfm) = 0
-[if] (AZBauto_pfm) == null
-    ($AZBauto_pfm) = 0
-[if](AZB_action) == null
-    ($AZB_action) = dazuo
 #input ($ZBWaitCD) = 从此次追捕开始，等待技能冷却,(ZBWaitCD)
 #input ($ZBcdskill) = 需要cd的技能使用英文逗号隔开或^不需要cd的技能,(ZBcdskill)
 #input ($ZBMax) = 最高连续追捕数达到后自动放弃,(ZBMax)
 #select ($DieToReset) = 死亡自动重置,已开启|已关闭,已关闭
-#input ($ZBeq) = 追捕开始换套装数字,(ZBeq)
-#input ($AZBeq) = 追捕结束换套装数字,(AZBeq)
-#input ($ZBskill) = 追捕开始换技能；顺序为拳脚内功轻功招架刀法剑法杖法棍法鞭法暗器,(ZBskill)
-#input ($AZBskill) = 追捕结束换技能；顺序为拳脚内功轻功招架刀法剑法杖法棍法鞭法暗器,(AZBskill)
-#input ($ZBauto_pfm) = 追捕开始换自动出招顺序,(ZBauto_pfm)
-#input ($AZBauto_pfm) = 追捕结束换自动出招顺序,(AZBauto_pfm)
 // 副本方式
 #select ($FbName) = 副本,温府|恒山|衡山|嵩山|桃花岛|星宿海|冰火岛|移花宫|燕子坞|黑木崖|缥缈峰|光明顶|天龙寺|血刀门|古墓派|,(FbName)
 #select ($FbDiff) = 副本难度,普通|困难,普通
 #select ($FbWay) = 刷本方式（选自动前先确定插件支持）,自动|扫荡,扫荡
 // 日常结束后动作
-#input ($AZB_action) = 日常结束后行为,(AZB_action)
+[if](ARC_action) == null
+    ($ARC_action) = $zdwk
+#input ($ARC_action) = 日常结束后行为,(ARC_action)
 #config
 // 停止自动喜宴和boss，记录获取物品
 <-stopSSAuto
@@ -133,26 +115,14 @@ $wait 10000
 stopstate
 //追捕前准备
 @cd
-[if](ZBeq) != 0
-    $eq (ZBeq)
-    $wait 1000
-($skill) = 'unarmed','force','dodge','parry','blade','sword','staff','club','whip','throwing'
-@js ($skill_num) = [(skill)].length
-($num)=0
-[while](num) < (skill_num)
-    @js ($base_skill) = [(skill)][(num)]
-    @js ($spe_skill) = [(ZBskill)][(num)]
-    [if](spe_skill) != 0
-        enable (base_skill) (spe_skill)
-    ($num) = (num) + 1
-[if](ZBauto_pfm) != 0
-    @js WG.Send("setting auto_pfm (ZBauto_pfm)")
 @cmdDelay 1000
 @toolbar tasks
 @task 追杀逃犯：目前完成($currentN)/20个，共连续完成($comboN)个|追杀逃犯：($empty)目前完成($currentN)/20个，共连续完成($comboN)个
+
 [while](currentN) < 20
     @renew
-    @until (:status xuruo) == false   
+    @until (:status xuruo) == false
+    
     [if](comboN)>=(ZBMax)
         $to 扬州城-衙门正厅
         ask1 {程药发}
@@ -160,181 +130,171 @@ stopstate
         ($comboN) = 0
     [if](comboN)>=(ZBWaitCD)
         @cd (ZBcdskill)
+
     $to 扬州城-衙门正厅
     ($olddir1) = (dir1)
     ($olddir2) = (dir2)
     @print (olddir1)
     ($escapee) = null
+    
     [while] (escapee) == null
         ask1 {程药发}
         @toolbar tasks
         @task 追杀逃犯：($escapee)，据说最近在($dir1)-($dir2)出现过，你还有($time)去寻找他，目前完成($currentN)/20个，共连续完成($comboN)个。|追杀逃犯：目前完成($currentN)/20个，共连续完成($comboN)个
+
     [if](olddir1) != (dir1) && (olddir2) != (dir2)
         ($start_h) = (:hour)
         ($start_m) = (:minute)
+
     $wait 500
+
     [while] {(escapee)}? == null
         <---
-        @cmdDelay 1000
-        [if] {(escapee)}? != null
-            ($type1) = null
-            kill {(escapee)}
-            @until {(escapee)的尸体}? != null | {r(escapee)}? == null | (:combating) == false
-            @tip 你的追捕任务完成了，目前完成($currentN)/20个，已连续完成($comboN)个。|你($type1)死了($type2)|你要攻击谁
-            relive
-            [if](type1)!= null
+            @cmdDelay 1000
+            [if] {(escapee)}? != null
+                ($type1) = null
+                kill {(escapee)}
+
+                @until {(escapee)的尸体}? != null | {r(escapee)}? == null | (:combating) == false
+                @tip 你的追捕任务完成了，目前完成($currentN)/20个，已连续完成($comboN)个。|你($type1)死($type2)|你要攻击谁
                 relive
-                [if](DieToReset) == 已开启
-                    $to 扬州城-衙门正厅
-                    ask2 {程药发}
-            [break]
-        [if] (DieToReset) == 已关闭
-            ($tb)=(start_h)*60-(:hour)*60-(start_m)+(:minute)
+                [if](type1)!= null
+                    relive
+                    [if](DieToReset) == 已开启
+                        $to 扬州城-衙门正厅
+                        ask2 {程药发}
+                [break]
+            [if] (DieToReset) == 已关闭
+                ($tb)=(start_h)*60-(:hour)*60-(start_m)+(:minute)
                 //@print 已耗时(tb)
                 [if](tb)>10
-                    [break]               
+                    [break]                    
         --->
         @cmdDelay 300
         $to (dir1)-(dir2)
         [if] (dir1) == 武当派
             [if](dir2) == 林间小径
                 go south
-                jh fam 1 start
+            jh fam 1 start
+            go north
+            go south;go west
+            go west
+            go east;go northup
+            go north
+            go east
+            go west;go west
+            go northup
+            go northup
+            go northup
+            go north
+            go north
+            go north
+            go north
+            go north
+            go north
+        [else if] (dir1) == 华山派
+            jh fam 3 start
+            go eastup
+            go southup
+            jumpdown
+            go southup
+            go south
+            go east
+            jh fam 3 start
+            go westup
+            go north
+            go east
+            go west;go north
+            go east
+            go west;go north
+            go south[3];go west
+            go east;go south
+            go southup
+            go southup
+            look bi;break bi;go enter
+            go westup
+            go westup
+            jumpup
+        [else if] (dir1) == 少林派
+            [if](dir2) == 竹林
+                go south
+            jh fam 2 start
+            go north
+            go west
+            go east;go east
+            go west;go north
+            go northup
+            go southdown;go northeast
+            go northwest
+            go southwest
+            go northeast;go north
+            go east
+            go west;go west
+            go east;go north
+            go east
+            go west;go west
+            go east;go north
+            go west
+            go east;go north
+            go north    
+        [else if] (dir1) == 峨眉派
+            [if](dir2) == 走廊
                 go north
+                go south[2]
+                go north;go east[2]
+            jh fam 4 start
+            go northup
+            go east
+            go west;go southdown;go west
+            go south
+            go east
+            go east
+            go west;go south
+            go north;go west;go south
+            go north;go west
+            go south
+            go south
+            go north;go north;go west
+            go east;go north
+            go north
+        [else if] (dir1) == 逍遥派
+            [if](dir2) == 林间小道
+                go west;go north
                 go south;go west
-                go west
-                go east;go northup
-                go north
-                go east
-                go west;go west
-                go northup
-                go northup
-                go northup
-                go north
-                go north
-                go north
-                go north
-                go north
-                go north
-            [else if] (dir1) == 华山派
-                jh fam 3 start
-                go eastup
-                go southup
-                jumpdown
-                go southup
-                go south
-                go east
-                jh fam 3 start
-                go westup
-                go north
-                go east
-                go west;go north
-                go east
-                go west;go north
-                go south[3];go west
                 go east;go south
-                go southup
-                go southup
-                look bi;break bi;go enter
-                go westup
-                go westup
-                jumpup
-            [else if] (dir1) == 少林派
-                [if](dir2) == 竹林
-                    go south
-                jh fam 2 start
-                go north
-                go west
-                go east;go east
-                go west;go north
-                go northup
-                go southdown;go northeast
-                go northwest
-                go southwest
-                go northeast;go north
-                go east
-                go west;go west
-                go east;go north
-                go east
-                go west;go west
-                go east;go north
-                go west
-                go east;go north
-                go north   
-            [else if] (dir1) == 峨眉派
-                [if](dir2) == 走廊
-                    go north
-                    go south[2]
-                    go north;go east[2]
-                jh fam 4 start
-                go northup
-                go east
-                go west;go southdown;go west
-                go south
-                go east
-                go east
-                go west;go south
-                go north;go west;go south
-                go north;go west
-                go south
-                go south
-                go north;go north;go west
-                go east;go north
-                go north
-            [else if] (dir1) == 逍遥派
-                [if](dir2) == 林间小道
-                    go west;go north
-                    go south;go west
-                    go east;go south
-                [else if](dir2) == 木屋
-                    go south[4]
-                [else if](dir2) == 地下石室
-                    go up
-                jh fam 5 start
-                go north
-                go north
-                jh fam 5 start;go east
-                go north
-                go south;go south
-                go south
-                jh fam 5 start;go west
-                go south
-                jh fam 5 start;go south
-                go south
-                jh fam 5 start;go down
-                go down
-            [else if] (dir1) == 丐帮
-                [if](dir2) == 暗道
-                    go east
-                    go east[2]
-                    go east
-                jh fam 6 start
-                go down
-                go east
-                go east
-                go east
+            [else if](dir2) == 木屋
+                go south[4]
+            [else if](dir2) == 地下石室
                 go up
-                go down;go east
+            jh fam 5 start
+            go north
+            go north
+            jh fam 5 start;go east
+            go north
+            go south;go south
+            go south
+            jh fam 5 start;go west
+            go south
+            jh fam 5 start;go south
+            go south
+            jh fam 5 start;go down
+            go down
+        [else if] (dir1) == 丐帮
+            [if](dir2) == 暗道
                 go east
-                go up
-@renew
-// 追捕后复原
+                go east[2]
+                go east
+            jh fam 6 start
+            go down
+            go east
+            go east
+            go east
+            go up
+            go down;go east
+            go east
+            go up
 @cmdDelay 1000
-@cd
-[if](AZBeq) != 0
-    $eq (AZBeq)
-    $wait 1000
-($num)=0
-[while](num) < (skill_num)
-    @js ($base_skill) = [(skill)][(num)]
-    @js ($spe_skill) = [(AZBskill)][(num)]
-    [if](spe_skill) != 0
-        enable (base_skill) (spe_skill)
-    ($num) = (num) + 1
-[if](AZBauto_pfm) != 0
-    @js WG.Send("setting auto_pfm (AZBauto_pfm)")
-@tidyBag
-$wait 10000
+@renew
 @cd
 // 副本
 [if] (FbName) == 温府
@@ -397,4 +357,4 @@ stopSSAuto->
 $eq 2
 @await 6000
 // 执行日常结束后动作
-(AZB_action)
+(ARC_action)
