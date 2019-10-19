440c9c2d76ca0585e2993b41d21c2ab0a4183c89
Maple Observer <mapleobserver@gmail.com>
2019-10-20 07:36:54 +0800

    更新脚本

---
+++ 吸珠子-raid.lua
@@ -0,0 +1,21 @@
+<-stopSSAuto
+use {b聚元珠}
+@tip 是否准备好了？
+wqcy ok
+@tip 你感觉四周的元气似乎有放缓的趋势，是否献祭你的内力？
+wqcy next
+@tip 你感觉四周的元气似乎有放缓的趋势，是否献祭你的内力？
+wqcy next
+@tip 你感觉四周的元气似乎有放缓的趋势，是否献祭你的内力？
+wqcy next
+@tip 你感觉四周的元气似乎有放缓的趋势，是否献祭你的内力？
+wqcy next
+@tip 你感觉四周的元气似乎有放缓的趋势，是否献祭你的内力？
+wqcy next
+@tip 你感觉四周的元气似乎有放缓的趋势，是否献祭你的内力？
+wqcy next
+@tip 恭喜你炼化成功，
+stopstate
+cr;cr over
+$zdwk
+stopSSAuto->
