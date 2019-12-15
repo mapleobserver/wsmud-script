06dfb3ff68704483996c7334484feb0d5de24a1a
Maple Observer <mapleobserver@gmail.com>
2019-12-15 09:54:17 +0800

    调整借技能的角色判断，方便自定义。

--- 逍遥借技能/借出夺魄.trigger.lua
+++ 逍遥借技能/借出夺魄.trigger.lua
@@ -2,21 +2,21 @@
 // 四区：白三三
 // 新提示信息触发
 // 关键词人名自己修改
-// 关键字：白三三想和你比试
-$stoppfm
-// 关闭所有起手触发
-@off 明玉夺魄
-setting auto_pfm 0
-// 人名自己修改
-($id) = {r白三三}?
-fight (id)
-@until (:combating) == true
-@await 1000
-@perform unarmed.duo
-@until (:combating) == false
-$startpfm
-// 开启相关触发
-@on 明玉夺魄
-@liaoshang
-@dazuo
-$zdwk
+// 关键字：想和你比试
+@js ($player) = "(text)".match("<hiy>([^%]+)想和你比试")[1]
+[if] (player) == 白三三 || (player) == 开门送温暖
+    $stoppfm
+    // 关闭所有起手触发
+    @off 明玉夺魄
+    setting auto_pfm 0
+    ($id) = {r(player)}?
+    fight (id)
+    @until (:combating) == true
+    @await 1000
+    @perform unarmed.duo
+    @until (:combating) == false
+    $startpfm
+    // 开启相关触发
+    @on 明玉夺魄
+    @renew
+    $zdwk
--- 逍遥借技能/借出无招.trigger.lua
+++ 逍遥借技能/借出无招.trigger.lua
@@ -2,22 +2,23 @@
 // 四区：白三三
 // 新提示信息触发
 // 关键词人名自己修改
-// 关键字：白三三想和你比试
-$stoppfm
-// 关闭所有起手触发
-@off 起手无招
-@off 补无招
-setting auto_pfm 0
-($id) = {r白三三}?
-fight (id)
-@until (:combating) == true
-@await 1000
-@perform sword.wu
-@until (:combating) == false
-// 开启相关起手触发
-@on 起手无招
-@on 补无招
-$startpfm
-@liaoshang
-@dazuo
-$zdwk
+// 关键字：想和你比试，你是否同意：
+@js ($player) = "(text)".match("<hiy>([^%]+)想和你比试")[1]
+[if] (player) == 白三三 || (player) == 开门送温暖
+    $stoppfm
+    // 关闭所有起手触发
+    @off 起手无招
+    @off 补无招
+    setting auto_pfm 0
+    ($id) = {r(player)}?
+    fight (id)
+    @until (:combating) == true
+    @await 1000
+    @perform sword.wu
+    @until (:combating) == false
+    // 开启相关起手触发
+    @on 起手无招
+    @on 补无招
+    $startpfm
+    @renew
+    $zdwk
