c4868c393655dad2658153cfb0d599522016e3df
Maple Observer <mapleobserver@gmail.com>
2019-12-27 09:29:42 +0800

    调整下武器过程的一些逻辑判断和顺序。

--- 帮派/峨眉工具人自动下武当武器.trigger.lua
+++ 帮派/峨眉工具人自动下武当武器.trigger.lua
@@ -10,24 +10,25 @@ $stoppfm;enable sword wudugoufa;enable staff shedaoqigong;enable throwing jinshe
 @on 无缝明玉
 @off 无缝太上
 @on 帮战翻车
-$to 武当派-后山小院
-@until (:room) == 武当派-后山小院
+[while] (:room) != 武当派-后山小院
+    $to 武当派-后山小院
+    @await 2000
 pty 准备下武器，大佬们帮忙清下号令
 ($id_1) = {r武当派长老}?
 ($id_2) = {r武当派长老}?# obj.id == "(id_1)" #
 ($id_bang) = {r张三丰}?
 ($id_fight) = (id_2)
+@liaoshang
 [while] true
     ($succes) = null
-    //$stoppfm
-    @liaoshang
+    @cd sword.suo,unarmed.duo
+    @print 开始比试
     fight (id_fight)
     @perform parry.yi,force.power,unarmed.duo,sword.suo
     @tip 当啷一声($succes)地上|手中并($succes)兵器|早有准备|你向后退了几步|这场比试算我输了|承让|脸色微变|向后跃开三尺|果然高明
-    [while] true
+    [while] (:room) == 武当派-后山小院
         go south
-        [if] (:room) != 武当派-后山小院
-            [break]
+        @await 500
     @until (:combating) == false
     @print 已比试
     go north
@@ -38,13 +39,12 @@ pty 准备下武器，大佬们帮忙清下号令
         [else if] (id_fight) == (id_1)
             ($id_fight) = (id_bang)
         [else]
-            @print 下武器失败，准备重试
             [break]
     [else]
+        @print 下武器失败，准备重试
         $to 扬州城-武庙
     @liaoshang
     $to 武当派-后山小院
-    @cd sword.suo,unarmed.duo
 pty 武器已下
 @liaoshang
 $usezml 参合
