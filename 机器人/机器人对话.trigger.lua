ca3bcbc78273033c94d265c443187e45150792f0
Maple Observer <mapleobserver@gmail.com>
2020-03-14 18:24:40 +0800

    优化机器人的判断和规范常量传递。

--- 机器人/机器人对话.trigger.lua
+++ 机器人/机器人对话.trigger.lua
@@ -1,15 +1,23 @@
 //raid.flow
 //四区白三三
-//世界频道关键字：33
+//世界频道关键字：33|jn
 //第一行机器人的名字自行修改
 [if] (name) != 村花三三
     [if] (SSstatus) == false || (SSstatus) == null
         ($SSinfo) = (content)
         ($SSname) = (name)
-        @js ($SSContent) = var ssStart='(content)'.match('^33');if(ssStart!=null){'(content)'.substr([2]).trim()}
-        [if] (SSContent) == null
+        @js ($ssStart) = '(content)'.match('^33')
+        @js ($jnStart) = '(content)'.match('^jn')
+        //@print (ssStart)
+        [if] (ssStart) != null
+            @js ($ssContent) = '(content)'.substr([2]).trim()
+            //@print (ssContent)
+            [if] (ssContent) == null
+                ($SSstatus) = true
+                @call 随机说话
+            [else]
+                ($SSstatus) = true
+                @call 资料查询
+        [if] (jnStart) != null
             ($SSstatus) = true
-            @call 随机说话
-        [else]
-            ($SSstatus) = true
-            @call 资料查询
+            @call 技能查询
