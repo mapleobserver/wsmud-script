5bb1df541fed6c3b2843d289817a0aae427e035b
Maple Observer <mapleobserver@gmail.com>
2021-02-06 15:25:28 +0800

    更新版本号

--- plugins/wsmud_Raid.user.js
+++ plugins/wsmud_Raid.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name            wsmud_Raid
 // @namespace       cqv
-// @version         2.4.33
+// @version         2.4.34
 // @date            23/12/2018
-// @modified        06/02/2020
+// @modified        06/02/2021
 // @homepage        https://greasyfork.org/zh-CN/scripts/375851
 // @description     武神传说 MUD
 // @author          Bob.cn, 初心, 白三三
