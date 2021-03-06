2652c947d4fd1cebca8626640fd94da47f9bbba9
Maple Observer <mapleobserver@gmail.com>
2021-03-06 17:15:34 +0800

    将推演提升数据排除在物品获得数据之外。

--- plugins/wsmud_pluginss.user.js
+++ plugins/wsmud_pluginss.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         wsmud_pluginss
 // @namespace    cqv1
-// @version      0.0.32.152
+// @version      0.0.32.153
 // @date         01/07/2018
-// @modified     15/02/2021
+// @modified     06/03/2021
 // @homepage     https://greasyfork.org/zh-CN/scripts/371372
 // @description  武神传说 MUD 武神脚本 武神传说 脚本 qq群367657589
 // @author       fjcqv(源程序) & zhzhwcn(提供websocket监听)& knva(做了一些微小的贡献) &Bob.cn(raid.js作者)
@@ -7049,7 +7049,8 @@
                 if (G.getitemShow) {
                     if (data.msg.indexOf("恭喜你得到") >= 0 ||
                         (data.msg.indexOf("获得") >= 0 &&
-                            data.msg.indexOf("经验") == -1) ||
+                            data.msg.indexOf("经验") == -1 &&
+                            data.msg.indexOf("提升") == -1) ||
                         data.msg.indexOf("你找到") == 0 ||
                         data.msg.indexOf("你从") == 0 ||
                         data.msg.indexOf("得到") >= 0) {
