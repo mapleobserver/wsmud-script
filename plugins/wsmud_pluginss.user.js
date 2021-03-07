8262630fbdbfba19ae558386400f250d1b4b1965
Maple Observer <mapleobserver@gmail.com>
2021-03-07 11:02:17 +0800

    更新懒人包路径；优化物品获得信息的过滤。

--- plugins/wsmud_pluginss.user.js
+++ plugins/wsmud_pluginss.user.js
@@ -2965,7 +2965,7 @@
                         let mlh = `// 导入三三懒人包流程，方便后续导入操作
                         // 自命令类型选 Raidjs流程
                         // 四区白三三
-                        ($f_ss)={"name":"三三懒人包","source":"http://wsmud-cdn.if404.com/三三懒人包.flow.txt","finder":"根文件夹"}
+                        ($f_ss)={"name":"三三懒人包","source":"https://cdn.jsdelivr.net/gh/mapleobserver/wsmud-script/三三懒人包.flow.txt","finder":"根文件夹"}
                         @js var time=Date.parse(new Date());var f=(f_ss);var n=f["name"];var s=f["source"];var fd=f["finder"];WorkflowConfig.removeWorkflow({"name":n,"type":"flow","finder":fd});$.get(s,{stamp:time},function(data,status){WorkflowConfig.createWorkflow(n,data,fd);});
                         @awiat 2000
                         tm 【三三懒人包】流程已导入，如果曾用早期版本的懒人包导入过流程，请先删除这些流程后再使用。`;
@@ -7050,6 +7050,8 @@
                     if (data.msg.indexOf("恭喜你得到") >= 0 ||
                         (data.msg.indexOf("获得") >= 0 &&
                             data.msg.indexOf("经验") == -1 &&
+                            data.msg.indexOf("潜能") == -1 &&
+                            data.msg.indexOf("郭靖") == -1 &&
                             data.msg.indexOf("提升") == -1) ||
                         data.msg.indexOf("你找到") == 0 ||
                         data.msg.indexOf("你从") == 0 ||
