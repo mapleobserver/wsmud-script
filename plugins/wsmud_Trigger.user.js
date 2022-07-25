5d0c72d6548f953c45417ec7467bfd796f7cdd51
MapleObserver <mapleobserver@gmail.com>
2022-07-25 20:27:19 +0800

    更新版本号

--- plugins/wsmud_Trigger.user.js
+++ plugins/wsmud_Trigger.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name            wsmud_Trigger
 // @namespace       cqv3
-// @version         0.0.44
+// @version         0.0.45
 // @date            03/03/2019
-// @modified        05/07/2022
+// @modified        25/07/2022
 // @homepage        https://greasyfork.org/zh-CN/scripts/378984
 // @description     武神传说 MUD
 // @author          Bob.cn, 初心, 白三三
