13859440cbbf79a484f5fdfe72e6dc56681163c7
MapleObserver <mapleobserver@gmail.com>
2023-01-18 11:23:23 +0800

    更新云备份url

--- plugins/wsmud_Raid.user.js
+++ plugins/wsmud_Raid.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name            wsmud_Raid
 // @namespace       cqv
-// @version         2.4.60
+// @version         2.4.61
 // @date            23/12/2018
-// @modified        28/10/2022
+// @modified        18/1/2023
 // @homepage        https://greasyfork.org/zh-CN/scripts/375851
 // @description     武神传说 MUD
 // @author          Bob.cn, 初心, 白三三
@@ -4573,7 +4573,7 @@ look men;open men
             });
         },
 
-        _address: "mush.fun/api",
+        _address: "mush.aize.org/api",
         _async(uri, params, success, fail) {
             this._get(true, uri, params, success, fail);
         },
